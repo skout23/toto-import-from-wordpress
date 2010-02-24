95c06d167960bfffae5877e6912da30391c2d151
Scott <skout@gambits.org>
2010-02-23 19:20:08 -0800

    removed my author, and added comments

--- import.rb
+++ import.rb
@@ -7,13 +7,12 @@ require 'toto'
 
 file = File.new(ARGV[0])
 
-# we have to hack the XML file, unfortunately, since it isn't valid
-# at least, for Wordpress 2.6.2
 doc = Hpricot( File.open(file) )
 
 (doc/"item").each do |item|
 if item.search("wp:post_type").first.inner_text == "post" and item.search("wp:status").first.inner_text == "publish" then
 
+#  I did not need to export comments, so I just ignored them for now, however here they are
 #  comments = item.search("wp:comment_approved").reject {|ct| ct.inner_text != "1" }.size
 #  pingbacks = item.search("wp:comment_type").reject {|ct| ct.inner_text != "pingback" }.size
   is_private = ( item.search("wp:status").first.inner_text == "private" )
@@ -36,13 +35,17 @@ if item.search("wp:post_type").first.inner_text == "post" and item.search("wp:st
     next
   end
   
+# If you use a differing format for the slug, you should change this strftime
   path = "./articles/#{time.strftime("%Y-%m-%d")}#{'-' + slug if slug}.txt"
   
   begin 
     newpost = File.open(path,'w')
     newpost.puts "---\ntitle: #{title.chomp}\n"
-    newpost.puts "author: Scott"
+# Should be able to pull your toto config here, but just in case I made it static for me.
+    newpost.puts "author: YOUR_AUTHOR"
+# If you use a differing format for the date, you might need to change this strftime
     newpost.puts "date: #{time.strftime("%d/%m/%Y")}"
+# if in the future toto handles tags or the like in a default manner I will update this accordingly
     #newpost.puts "tags: #{tags}\n" if !tags.nil? and !tags.empty?
     newpost.puts "\n\n\n"
     newpost.puts content
