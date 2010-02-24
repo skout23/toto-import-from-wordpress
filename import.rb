be6fce9336fea668370c936168f57faa6d419d3d
Scott <skout@gambits.org>
2010-02-23 19:08:36 -0800

    initial import of my changes

--- import.rb
+++ import.rb
@@ -1,61 +1,60 @@
+#!/usr/bin/env ruby
+
 require 'rubygems'
-require 'rexml/document'
+require 'hpricot'
 require 'time'
-require 'sequel'
-
-# Change this to load the DB for your blog
-DB = Sequel.connect('sqlite://blog.db')
-
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
-require 'post'
-
-# This allows us to overwrite the primary key, so that you
-# can keep the same links for your comments
-Post.unrestrict_primary_key
-
-if ARGV.length == 0
-  puts 'I require an XML file name to be passed as an argument.'
-  exit 1
-end
+require 'toto'
 
 file = File.new(ARGV[0])
 
 # we have to hack the XML file, unfortunately, since it isn't valid
 # at least, for Wordpress 2.6.2
-file = file.read
-file.sub!(/xmlns:wp="http:\/\/wordpress.org\/export\/1.0\/"/,
-         "xmlns:wp=\"http://wordpress.org/export/1.0/\"\nxmlns:excerpt=\"excerpt\"")
+doc = Hpricot( File.open(file) )
 
-doc = REXML::Document.new file
+(doc/"item").each do |item|
+if item.search("wp:post_type").first.inner_text == "post" and item.search("wp:status").first.inner_text == "publish" then
 
-# Assume that there is one channel  (FIX?)
-# cycle through all of the items
-doc.root.elements["channel"].elements.each("item") { |item| 
-  # if it's a published post, then we import it
-  # Scanty doesn't support pages or drafts yet
-  if item.elements["wp:post_type"].text == "post" and
-     item.elements["wp:status"].text == "publish" then
-     
-		post_id = item.elements["wp:post_id"].text.to_i
-    title = item.elements["title"].text
-    content = item.elements["content:encoded"].text
-    time = Time.parse item.elements["wp:post_date"].text
-		# post_parent = item.elements["wp:post_parent"].text.to_i
-    tags = []
-    item.elements.each("category") { |cat|
-      domain = cat.attribute("domain")
-      if domain and domain.value == "tag"
-        tags.unshift cat.text
-      end
-    }
-    tags = tags.map { |t| t.downcase }.sort.uniq
-
-    post = Post.new :id => post_id, :title => title, :tags => tags.join(' '), :body => content, :created_at => time, :slug => Post.make_slug(title)
-    if post.save
-      puts "Saved post: id ##{post.id} #{title}"
-    else
-      puts "ERROR! could not save post #{title}"
-      exit
-    end
+#  comments = item.search("wp:comment_approved").reject {|ct| ct.inner_text != "1" }.size
+#  pingbacks = item.search("wp:comment_type").reject {|ct| ct.inner_text != "pingback" }.size
+  is_private = ( item.search("wp:status").first.inner_text == "private" )
+#  has_comments = ( comments > 0 && comments > pingbacks )
+  tags = item.search("category[@domain='tag']").collect(&:inner_text).uniq
+  tags = tags.map { |t| t.downcase }.sort.uniq
+  
+  next if item.search("wp:post_type").first.inner_text != "post"
+  
+  post_id = item.search("wp:post_id").first.inner_text.to_i
+  title = item.search("title").first.inner_text.gsub(/:/, '')
+  slug = title.empty?? nil : title.strip.slugize
+  time = Time.parse item.search("wp:post_date").first.inner_text
+  link = item.search("link").first.inner_text
+  
+  content = item.search("content:encoded").first.inner_text.to_s
+ 
+  if content.strip.empty?
+    puts "Failed to parse postId #{post_id}:#{title}"
+    next
   end
-}
+  
+  path = "./articles/#{time.strftime("%Y-%m-%d")}#{'-' + slug if slug}.txt"
+  
+  begin 
+    newpost = File.open(path,'w')
+    newpost.puts "---\ntitle: #{title.chomp}\n"
+    newpost.puts "author: Scott"
+    newpost.puts "date: #{time.strftime("%d/%m/%Y")}"
+    #newpost.puts "tags: #{tags}\n" if !tags.nil? and !tags.empty?
+    newpost.puts "\n\n\n"
+    newpost.puts content
+  rescue Exception => e  
+    puts e.message  
+    puts e.backtrace.inspect  
+    puts "ERROR! could not save post #{title}"
+    exit
+  end  
+end
+end
+
+
+
+
